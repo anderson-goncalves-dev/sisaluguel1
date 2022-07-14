852236a1ff90a8acd6be64318e8a99f530ae95d0
Anderson Gonçalves <81239923+Anderson191225@users.noreply.github.com>
2022-07-14 16:27:27 -0300

    Update config.php

--- sistema/aluguel/config.php
+++ sistema/aluguel/config.php
@@ -3,7 +3,7 @@
 
 ##arquivo que contem variaveis que serão usadas no arquivo de configuração do BD
 
-define('HOST', 'http://10.70.200.253:8081/');
+define('HOST', '10.70.200.253:8081');
 define('USER', 'sisaluguel');
 define('PASS', 'sisaluguel2021');
 define('BASE', 'sisaluguel');
